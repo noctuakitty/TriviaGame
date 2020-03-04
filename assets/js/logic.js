c82aebe1a2bdc0ac5dcf7f4aba4ddc0cc86042b3
Kilea Castillo <kileaw@gmail.com>
2020-03-04 15:21:57 -0700

    everything is broken

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,198 +1,225 @@
 $(document).ready(function () {
+    stop()
+    
+    //Questions Array
+
     var options = [
     {
-         question: "How many planets are in our solar system?", 
-         choice: ["5", "7", "8", "9"],
-         answer: 2,
-        photo: "assets/imgs/"
+        question: "How many planets are in our solar system?", 
+        choice: ["5", "7", "8", "9"],
+        correctAnswer: "8",
+        photo: "assets/imgs/solar_system.png"
     },
     {
-         question: "How many natural satellites does the Earth have?", 
+        question: "How many natural satellites does the Earth have?", 
         choice: ["1", "2", "24", "None"],
-        answer: 0,
-        photo: "assets/imgs/"
+        correctAnswer: "1",
+        photo: "assets/imgs/fullmoon.jpg"
     }, 
     {
         question: "Most of the planets in our solar system are named after what?", 
         choice: ["Famous astronomers", "Greek/Roman Gods", "Norse Gods", "Stars" ],
-        answer: 1,
-        photo: "assets/imgs/"
+        correctAnswer: "Greek/Roman Gods",
+        photo: "assets/imgs/greek-mythology.jpg"
     }, 
     {
         question: "'The Red Planet' is the poetic name for ... ?", 
         choice: ["Jupiter", "Mars", "Saturn", "Venus" ],
-        answer: 1,
-        photo: "assets/imgs/"
+        correctAnswer: "Mars",
+        photo: "assets/imgs/mars.jpg"
     }, 
     {
         question: "What's a group of stars that resemble an earthly object called?",
         choice: ["A constellation", "A nebula", "A coma stellata", "A galaxy" ],
-        answer: 0,
-        photo: "assets/imgs/"
+        correctAnswer: "A constellation",
+        photo: "assets/imgs/constellations.jpg"
     }, 
     {
         question: "Which planet has the Great Red Spot?",
         choice: ["Jupiter", "Neptune", "Uranus", "Mercury" ],
-        answer: 0,
-        photo: "assets/imgs/"
+        correctAnswer: "Jupiter",
+        photo: "assets/imgs/jupiter.jpg"
     }, 
     {
         question: "Saturn is best known for its gorgeous ______.", 
         choice: ["Sunrises", "Mountains", "Rings", "Craters" ],
-        answer: 2,
-        photo: "assets/images/lemon.gif"
+        correctAnswer: "Rings",
+        photo: "assets/imgs/saturn.jpg"
     }, 
     {
         question: "Is the Earth considered an astronomical body?", 
         choice: ["Yes, it is.", "No, it is not."],
-        answer: 0,
-        photo: "assets/imgs/"
+        correctAnswer: "Yes, it is.",
+        photo: "assets/imgs/earth.jpg"
     },
     {
         question:"A ball of ice and dust with a long, glowing tail is called what?",
         choice: ["An astroid", "A blue dwarf", "A meteor", "A comet"],
-        answer: 3,
-        photo:"assets/imgs/"
+        correctAnswer: "A comet",
+        photo:"assets/imgs/comet.jpg"
     },
     {
         question: "What's the name of the galaxy we live in?",
-        choice: ["The Andromeda", "The Milkey Way", "The Fishnet", "The Zarathusthra"],
-        answer: 1,
-        photo: "assets/imgs/"
+        choice: ["The Andromeda", "The Milky Way", "The Fishnet", "The Zarathusthra"],
+        correctAnswer: "The Milky Way",
+        photo: "assets/imgs/milky_way.jpg"
     },
     {
         question: "True or false: Ursa Major and the Big Dipper are the same thing.", 
         choice: ["True", "False"],
-        answer: 1,
-        photo: "assets/imgs/"
+        correctAnswer: "False",
+        photo: "assets/imgs/ursa-major.png"
     },
     {
         question: "Which of these four planets is not a 'terrestrial planet'?",
         choice: ["Earth", "Mars", "Mercury", "Saturn"],
-        answer: 3,
-        photo: "assets/imgs/"
+        correctAnswer: "Saturn",
+        photo: "assets/imgs/saturn.jpg"
     },
     {
         question: "Beyond the terrestrial planets, we find ... ?",
         choice: ["Gas giants", "Ice giants", "Exoplanets", "Both gas and ice giants"],
-        answer: 3,
-        photo: "assets/imgs/"
+        correctAnswer: "Both gas and ice giants",
+        photo: "assets/imgs/ice-and-gas-giants.jpg"
     },
     {
         question: "On which astronomical object would you find the Sea of Tranquility?",
         choice: ["The moon", "Mars", "The sun", "Jupiter"],
-        answer: 0,
-        photo: "assets/imgs/"
+        correctAnswer: "The moon",
+        photo: "assets/imgs/fullmoon.jpg"
     },
     {
         question: "When a large star collapses under its own weight, what does it become?",
         choice: ["A blue dwarf", "A red giant", "A nebula", "A black hole"],
-        answer: 3,
-        photo: "assets/imgs/"
+        correctAnswer: "A black hole",
+        photo: "assets/imgs/black-hole.jpg"
     },
     {
         question: "Which planet's moons are named (in translation) 'fear' and 'dread'?",
         choice: ["Jupiter", "Mars", "Neptune", "Mercury"],
-        answer: 1,
-        photo: "assets/imgs/"
+        correctAnswer: "Mars",
+        photo: "assets/imgs/mars.jpg"
     },
     {
         question: "What is an exoplanet?",
         choice: ["The molten core of a planet", "A planet outside the solar system", "Floating rubble that once was a planet", "This is just a term from science fiction."],
-        answer: 1,
-        photo: "assets/imgs/"
+        correctAnswer: "A planet outside the solar system",
+        photo: "assets/imgs/exoplanet.jpg"
     },
     {
         question: "When we talk about a star's brightness, we refer to its what?",
         choice: ["Lumosity", "Beaufort-scale position", "Wattage", "Magnitude"],
-        answer: 3,
-        photo: "assets/imgs/"
+        correctAnswer: "Magnitude",
+        photo: "assets/imgs/sun.jpg"
     },
     {
         question: "A light-year is a measure of what?",
         choice: ["Brightness", "Distance", "Time", "Size"],
-        answer: 1,
-        photo: "assets/imgs/"
+        correctAnswer: "Distance",
+        photo: "assets/imgs/light-year.jpg"
     }
+   ]
 
+   var number = 20;
+   var userChoice = "";
+   var intervalId;
+   var winCount = 0;
+   var loseCount = 0; 
+   var correctAnswer = options.correctAnswer;
 
-    ]
+   $(".card-header").hide();
+   $(".card-body").hide();
+   $("#list").hide();
+   $("#question").hide();
+   $("#timer").hide();
+   $(".picture").hide();
 
-    var number = 20;
-    var userChoice = "";
-    var intervalId;
-    var winCount = 0;
-    var loseCount = 0; 
+    $("#playbutton").click (function(play){
+     $(".card-header").show();
+     $(".card-body").show();
+     $("#list").show();
+     $("#question").show();
+     $("#timer").show();
+   });
+ 
+   $("#play").on("click", run,);
 
+   function run() {
+     number=20;
+     intervalId = setInterval(decrement, 1000);
+     displayQuestion()
+   }
 
+   function decrement() {
+
+     number--;
+
+     $("#timer").html("<h2>" + number + "</h2>");
+
+     if (number === 0) {
+
+       stop();
+
+       alert("Time's Up! The correct answer is " + options.correctAnswer);
+       
+       run()
+     }
+     
+   }
+
+   function stop() {
+
+     clearInterval(intervalId);
+   }
+
+   run();
+
+   function displayQuestion (){
+       var i = Math.floor (Math.random()*options.length);
+       var q = options[i];
+       console.log(q)
+       $("#question").text(q.question);
+       $("#list").empty();
+       
+       for (var i = 0; i < q.choice.length; i++){
+           var item = $("<button>").text(q.choice[i]);
+           $("#list").append(item);
+       }
+
+       $("#list").on("click", pick,);
+       
+   }
+
+   function pick () {
+        if (clickedAnswer === correctAnswer) {
+        console.log("right")
+        }else {
+        console.log("wrong")
+    }
+   }
+    
+
+    function reset() {
     $(".card-header").hide();
     $(".card-body").hide();
     $("#list").hide();
     $("#question").hide();
     $("#timer").hide();
-
-   $("#playbutton").click (function(play){
-    $(".card-header").show();
-    $(".card-body").show();
-    $("#list").show();
-    $("#question").show();
-    $("#timer").show();
-    });
-  
-    $("#play").on("click", run,);
-
-    function run() {
-      number=20;
-      intervalId = setInterval(decrement, 1000);
-      displayQuestion()
-    }
-
-    function decrement() {
-
-      number--;
-
-      $("#timer").html("<h2>" + number + "</h2>");
-
-      if (number === 0) {
-
-        stop();
-
-        alert("Time Up!");
-        
-        run()
-      }
-      
-    }
-
-    function stop() {
-
-      clearInterval(intervalId);
-    }
-
-    run();
-
-    function displayQuestion (){
-        var i = Math.floor (Math.random()*options.length);
-        var q = options[i];
-        console.log(q)
-        $("#question").text(q.question);
-        $("#list").empty();
-        
-        for (var i = 0; i < q.choice.length; i++){
-            var item = $("<li>").text(q.choice[i]);
-            $("#list").append(item);
-
-        }
-          
-
-        
+    $(".picture").hide();
+    $("#resetbutton").hide();
 
     }
 
-// {
-//         question: "Which planet's moons are named (in translation) 'fear' and 'dread'?",
-//         choice: ["Jupiter", "Mars", "Neptune", "Mercury"],
-//         answer: 1,
-//         photo: "assets/imgs/"
-//     },
+
+
+
+
+
+
+
+
+
+
+
+
 });
